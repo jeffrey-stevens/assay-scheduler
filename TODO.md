c15bc8ee2beb24318f04d821d8a04f6abc26c5b0
Jeffrey Stevens <47906337+jeffrey-stevens@users.noreply.github.com>
2019-04-13 23:09:10 -0400

    Added Gantt charting.

---
+++ TODO.md
@@ -0,0 +1,5 @@
+# Todo
+
+* Rename the "vars" function so it isn't masked by ggplot2.
+* Remove the horizontal grid lines?
+* Add a title to the ggplot.
---
+++ gantt-chart.R
@@ -0,0 +1,64 @@
+################################################################################
+# gantt-chart.R
+# 
+# Jeff Stevens
+#
+#
+# Description
+# -----------
+#
+# A Gantt-charting function for multi-plate assay workflows.
+################################################################################
+
+
+library(ggplot2)
+
+
+gantt_chart <- function(timings, by = "plate") {
+  # timings:  Data frame with columns named "Plate", "Resource", "Start", and
+  # "Stop".  "Resource" should be an ordered factor.
+  #
+  # by: What to plot on the y-axis ("plate" for plate number, or "resource" for
+  # the resource used).
+  
+  # Convert times to minutes
+  timings <-
+    timings %>%
+    mutate(Start = Start / 60, Stop = Stop / 60)
+  
+  if ( identical(by, "plate") ) {
+    
+    breaks <- order(unique(timings$Plate))
+    labels <- as.character(as.integer(breaks))
+    
+    p <- 
+      ggplot(timings) +
+        geom_rect( aes( xmin = Start, xmax = Stop,
+                        ymin = Plate - 0.5, ymax = Plate + 0.5,
+                        fill = Resource) ) +
+        scale_y_continuous( breaks = breaks, labels = labels) +
+        xlab("\nTime (minutes)") + ylab("Plate\n") +
+        theme_bw()
+    
+  } else if ( identical(by, "resource") ) {
+    
+    breaks <- sort(unique(as.integer(timings$Resource)))
+    labels <- levels(timings$Resource)
+    
+    p <- 
+      ggplot(timings) +
+        geom_rect( aes( xmin = Start, xmax = Stop,
+                        ymin = as.integer(Resource) - 0.5, ymax = as.integer(Resource) + 0.5,
+                        fill = Plate) ) +
+        scale_y_continuous( breaks = breaks, labels = labels) +
+        xlab("\nTime (minutes)") + ylab("Resource\n") +
+        theme_bw()
+    
+  } else {
+    
+    stop("'by' must either be 'plate' or 'resource'.")
+  }
+  
+  
+  return(p)
+}
